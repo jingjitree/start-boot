6b0b553bd2bb7886a2be252a0821083a85480396
jingjitree <jingjitree@gmail.com>
2020-02-28 19:54:17 +0800

    项目配置修改

--- src/main/java/top/inson/springboot/StartBootApplication.java
+++ src/main/java/top/inson/springboot/StartBootApplication.java
@@ -3,12 +3,18 @@ package top.inson.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StartBootApplication {
+public class StartBootApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(StartBootApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StartBootApplication.class);
+    }
 }
