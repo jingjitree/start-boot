47ca7d514880f189069d518d794a83c3fde1a01d
jingjitree <jingjitree@gmail.com>
2020-02-29 11:57:46 +0800

    druid数据源监控

--- src/main/java/top/inson/springboot/core/DruidConfiguration.java
+++ src/main/java/top/inson/springboot/core/DruidConfiguration.java
@@ -1,7 +1,11 @@
 package top.inson.springboot.core;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +19,29 @@ public class DruidConfiguration {
     public DataSource druidDataSource(){
         return new DruidDataSource();
     }
+
+    @Bean
+    public ServletRegistrationBean<StatViewServlet> druidStatViewServlet(){
+        ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+        //白名单
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
+        //黑名单
+        //servletRegistrationBean.addInitParameter("deny", "");
+        servletRegistrationBean.addInitParameter("loginUsername","admin");
+        servletRegistrationBean.addInitParameter("loginPassword","123456");
+        servletRegistrationBean.addInitParameter("resetEnable","false");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<WebStatFilter> druidStatFilter(){
+        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<>(new WebStatFilter());
+        //添加过滤规则
+        filterRegistrationBean.addUrlPatterns("/*");
+        //添加不需要忽略的格式信息
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico");
+        return filterRegistrationBean;
+    }
+
 
 }
