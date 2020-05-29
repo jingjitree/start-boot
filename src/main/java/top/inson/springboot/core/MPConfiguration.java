dd143ff0018c90f63b50cbd0546010e907770766
jingjitree <jingjitree@gmail.com>
2020-05-29 16:43:35 +0800

    mp的分页

--- src/main/java/top/inson/springboot/core/MPConfiguration.java
+++ src/main/java/top/inson/springboot/core/MPConfiguration.java
@@ -1,11 +1,22 @@
 package top.inson.springboot.core;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @MapperScan(value = "top.inson.springboot.dao")
 public class MPConfiguration {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        PaginationInterceptor interceptor = new PaginationInterceptor();
+
+
+        //开启count的join优化，只针对部分left join
+        return interceptor;
+    }
 
 
 
