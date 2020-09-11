a30e23b7997bf330731d3a815c65522f2af796b4
jingjitree <jingjitree@gmail.com>
2020-09-11 18:23:44 +0800

    swagger配置文件

---
+++ src/main/java/top/inson/springboot/core/SwaggerConfiguration.java
@@ -0,0 +1,40 @@
+package top.inson.springboot.core;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Log4j2
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+
+    @Bean
+    public Docket docket(Environment environment){
+        //设置要显示的swagger环境
+        Profiles profiles = Profiles.of("dev", "prod");
+        //通过Environment.acceptsProfiles 判断是否处在自己设定的环境中
+        boolean enable = environment.acceptsProfiles(profiles);
+        log.info("是否开启swagger：{}", enable);
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                //.groupName("开源boot")
+                .enable(enable)
+                .select()
+                //配置要扫描接口的方式
+                .apis(RequestHandlerSelectors.any())
+                //过滤什么路径下的接口
+                //.paths(PathSelectors.ant("/path/**"))
+                .build();
+    }
+
+
+}
